4f184fadaaa9086f08f829114c5408918abc77a0
Sandra Garzon <sandramgs7@gmail.com>
2022-06-27 20:53:16 -0500

    primera version de prueba

---
+++ assets/js/code.js
@@ -0,0 +1,8 @@
+const imgSlider = (anything) => {
+  document.querySelector(".pepsi").src = anything;
+};
+
+const changeBgColor = (color) => {
+  const sec = document.querySelector(".sec");
+  sec.style.background = color;
+};
